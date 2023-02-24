17c7d0d999aee7df0f764befc700b51b871d5573
Nidhin Krishna <nidhingopalank@gmail.com>
2023-02-24 15:41:01 +0530

    create,update,delete view completed

--- Todo/base/urls.py
+++ Todo/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
-from .views import TaskList,TaskDetail
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete
 
 urlpatterns = [
     path('',TaskList.as_view(),name='tasks'),
-    path('task/<int:pk>/',TaskDetail.as_view(),name='task')
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('create-task/',TaskCreate.as_view(), name='create'), 
+    path('update-task/<int:pk>/',TaskUpdate.as_view(), name='update'),
+        path('delete-task/<int:pk>/',TaskDelete.as_view(), name='delete'),
     
 ]
