9434dbc1edf53d11689da4b02429a20bf4d62b50
Nidhin Krishna <nidhingopalank@gmail.com>
2023-02-28 19:04:46 +0530

    new updations on register and login page

--- Todo/Todo/settings.py
+++ Todo/Todo/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
